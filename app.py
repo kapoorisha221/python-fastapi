2aa64d419cb3f65c28f1bf3c61e9e4d823795486
Maharshi Jasraj <maharshi.jasraj@celebaltech.com>
2024-05-27 11:45:43 +0530

    added /getfile api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import os, uvicorn, time
+import os, uvicorn, time, json
 from fastapi import FastAPI, UploadFile,BackgroundTasks, File, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.responses import JSONResponse
 from datetime import datetime
 
 from logs.logger import get_Error_Logger, get_Info_Logger, log_Garbage_Collector
@@ -11,19 +11,15 @@ from config.config import LocalConfig
 
 app = FastAPI()
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 info_logger = get_Info_Logger()
 error_logger = get_Error_Logger()
-
 
 
 def Process_Audio_files(agent_name, call_date):
@@ -35,9 +31,6 @@ def Process_Audio_files(agent_name, call_date):
     except Exception as e:
          error_logger.error(msg="An Error Occured ..",exc_info=e,extra={"location":"App.py - Process_Audio_files"})
     
-# @app.get("/")
-# def redirect_to_docs():
-#      return RedirectResponse("/docs")
 
 @app.post("/uploadfile/")
 async def create_upload_file(
@@ -55,22 +48,13 @@ async def create_upload_file(
                 status_code=422,
                 detail="Invalid date format, expected YYYY-MM-DD"
             )
-
         # Process files
         for file in files:
             with open(os.path.join(LocalConfig().RAW_DATA_FOLDER, file.filename), "wb") as f:
                 contents = await file.read()
                 f.write(contents)
 
-        # Schedule background task
-        s_time = time.time()
-        #print("start time__________________________",s_time)
         Process_Audio_files(agent_name, date_str)
-        e_time = time.time()
-        #print("End time__________________________",e_time)
-        Time_taken = e_time - s_time
-        #print("time__________________________",Time_taken)
-        #background_tasks.add_task(Process_Audio_files, agent_name, date_str)
         info_logger.info(msg="got the files and scheduled Background Task", extra={"location": "app.py-create_upload_file"})
         return {"message": "Got the files, will start Processing them Now!!!!"}
 
@@ -78,22 +62,40 @@ async def create_upload_file(
         error_logger.error(msg="An Error Occurred at post request..", exc_info=e, extra={"location": "app.py-create_upload_file"})
         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
 
-# @app.get("/createreport")
-# async def create_powerbi_reports():
-#     try:
-        
-#           pass
-#     except Exception as e:
-#         error_logger.error(msg="An Error Occured at get request..",exc_info=e,extra={"location":"App.py-create_upload_file"})
-#         return {"message": "An Error Occured while collecting the files!!"}
+
+
+
+@app.post("/get_file/")
+async def get_file(folder_name: str = Form(...)):
+    file_name = "power_bi_merged_output.json"
+    folder_path = LocalConfig().DATA_FOLDER + "/" +"audio_analytics" +"/"+ folder_name 
+    file_path = os.path.join(folder_path, file_name)
+    file_path = folder_path + "/" + file_name
+
+    if not os.path.exists(folder_path):
+        raise HTTPException(status_code=404, detail="Folder not found")
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    try:
+        with open(file_path, 'r') as file:
+            data = file.read()
+            json_data = json.loads(data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
+
+    return JSONResponse(content=json_data)
+
+
+
           
 
 # from pyngrok import ngrok  
- 
 # ngrok.set_auth_token("2al4KKug9Lj9a6NNZxArDX0BMNH_618BhSscx74HW56SR4RAM")
- 
 # public_url = ngrok.connect(8000)
 # print(f"FastAPI application is accessible at: {public_url}")
+
 
 if __name__ == "__main__":
     uvicorn.run(app)

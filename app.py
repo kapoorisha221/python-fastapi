29c9d5de80e1bec943db9872c83d1d7eda4cb202
Anshul Bhardwaj <anshul.bhardwaj@celebaltech.com>
2024-06-03 11:30:27 +0530

    Added Logging and created a file sample

--- app.py
+++ app.py
@@ -66,46 +66,65 @@ async def create_upload_file(
 
 @app.get("/readallaudios")
 def read_all_audios():
-    audio_source_path = ""
+    try:
+        info_logger.info(msg="Called the route '/readallaudios' and activated the function 'read_all_audios'", extra={"location": "app.py - read_all_audios"})
+        audio_source_path = ""
+    except Exception as e:
+        error_logger.error(msg="An Error Occurred at post request..", exc_info=e, extra={"location": "app.py - read_all_audios"})
+        raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
+        
     
 
 @app.post("/getfiledata/")
 async def get_file(file_lang: str= Form(...),folder_name: str = Form(...)):
-    if file_lang == "ar":
-        file_name = "arabic_power_bi_merged_output.json"
-    elif file_lang =="en":
-        file_name = "power_bi_merged_output.json"
-
-    folder_path = LocalConfig().DATA_FOLDER + "/" +"audio_analytics" +"/"+ folder_name 
-    file_path = os.path.join(folder_path, file_name)
-    file_path = folder_path + "/" + file_name
-
-    if not os.path.exists(folder_path):
-        raise HTTPException(status_code=404, detail="Folder not found")
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
+    info_logger.info(msg="Called the route '/getfiledata/' and activated the function 'get_file'", extra={"location": "app.py - get_file"})
 
     try:
-        # with open(file_path, 'r') as file:
-        #     data = file.read()
-        #     json_data = json.loads(data)
-        with open(file=file_path , mode="r", encoding="utf-8") as fh:
-            json_data = json.load(fh)
+        if file_lang == "ar":
+            file_name = "arabic_power_bi_merged_output.json"
+        elif file_lang =="en":
+            file_name = "power_bi_merged_output.json"
+
+        folder_path = LocalConfig().DATA_FOLDER + "/" +"audio_analytics" +"/"+ folder_name 
+        file_path = os.path.join(folder_path, file_name)
+        file_path = folder_path + "/" + file_name
+        
+        info_logger.info(msg=f"Accesing the audio_analytic folder files using path", extra={"location": "app.py - get_file"})
+
+        if not os.path.exists(folder_path):
+            raise HTTPException(status_code=404, detail="Folder not found")
+        if not os.path.exists(file_path):
+            raise HTTPException(status_code=404, detail="File not found")
+
+        try:
+            info_logger.info(msg=f"Aopeneing the Accessed the file  '{file_path}'", extra={"location": "app.py - get_file"})
+
+            with open(file=file_path , mode="r", encoding="utf-8") as fh:
+                json_data = json.load(fh)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
+
+        info_logger.info(msg=f"Returning the Data(Json) back to the user", extra={"location": "app.py - get_file"})
+        return JSONResponse(content=json_data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
-
-    return JSONResponse(content=json_data)
-
+        error_logger.error(msg="An Error Occurred at post request..", exc_info=e, extra={"location": "app.py - get_file"})
+        raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
 
 @app.get("/getaudiolist")
 async def get_audio_list():
+    info_logger.info(msg="Called the route '/getaudiolist' and activated the function 'get_audio_list'", extra={"location": "app.py - get_audio_list"})
     audio_data_path = "data/audios_info/mappings.json"
     try:
+        info_logger.info(msg="Reading the mapping.json and loading data as json", extra={"location": "app.py - get_audio_list"})
+
         with open(audio_data_path, "r") as mapping:
             data = mapping.read()
             json_data = json.loads(data)
     except Exception as e:
+        error_logger.error(msg="An Error Occurred at post request..", exc_info=e, extra={"location": "app.py - get_audio_list"})
         raise HTTPException(status_code=500, detail=f"Error reading file: {str(e)}")
+    
+    info_logger.info(msg="Returning the Json back to the user", extra={"location": "app.py - get_audio_list"})
     return JSONResponse(content=json_data)
           
 
---
+++ sample-pvtendpoint.py
@@ -0,0 +1,1 @@
+speech_service_rest_url = "https://my-private-link-speech.cognitiveservices.azure.com/speechtotext/v3.1/transcriptions"
